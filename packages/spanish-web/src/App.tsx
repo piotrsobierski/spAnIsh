482837ab1296aec37e404b576730cf62b688a120
piotrsobierski <p.sobierski@mservices.com.pl>
2024-11-26 20:51:00 +0100

    feat(spanish-web): Add simple header, get rid of CRA main page

--- packages/spanish-web/src/App.tsx
+++ packages/spanish-web/src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Header } from "./components/Header";
 import "./App.css";
+
+const theme = createTheme({
+  // You can customize your theme here
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Header />
+        {/* Your other content goes here */}
+      </div>
+    </ThemeProvider>
   );
 }
 
